928f82839cc0285ff4343cf1de54c6badd13d620
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-06-13 15:28:13 -0400

    update

--- next/src/app/page.js
+++ next/src/app/page.js
@@ -2,112 +2,42 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <main>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <section>
+      In the realm of sports, rivalries ignite passion, stirring the hearts of fans and captivating audiences worldwide. Today, we bear witness to a colossal clash that will reverberate through the annals of sporting history. Two behemoths of athletic prowess are set to collide on the hallowed grounds, creating an atmosphere electrified with anticipation. Brace yourselves as we delve into the exhilarating narrative that will unfold in this ultimate showdown.
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+Chapter 1: The Backstory
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+Our story commences with the dawn of a new era in sports. The renowned city of Stellarville, previously heralded for its unparalleled football dominance, has found itself dethroned by a brash newcomer—Havenwood. This aspiring sports powerhouse, a city replete with a burning hunger for victory, possesses an unyielding determination to rewrite the narrative of sporting glory.
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+Chapter 2: The Rise of Stellarville
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+Stellarville, the reigning champion, had long basked in the glory of success. The city's football team, the Stellar Strikers, forged an indomitable legacy etched in gold. With a roster of seasoned veterans and rising stars, they defied the odds and triumphed time and again, securing championship after championship. Stellarville was accustomed to victory, reveling in the adulation of fans who draped themselves in the city's triumphant colors.
+
+Chapter 3: The Ascent of Havenwood
+
+However, all empires face a challenge to their dominance, and Havenwood emerged as the beacon of change. Led by the brilliant tactician, Coach Maximus, and bolstered by an infusion of prodigious talent, Havenwood Thunderbolts struck fear into the hearts of their opponents. Their meteoric rise was impossible to ignore, as they claimed one victory after another, leaving a trail of vanquished foes in their wake. The city rallied behind their beloved team, displaying an unwavering loyalty that fueled their rise to prominence.
+
+Chapter 4: A Clash of Titans
+
+The stage is now set for a titanic showdown between Stellarville and Havenwood. The anticipation is palpable as these two powerhouses prepare to collide head-on. The air crackles with tension, spectators buzzing with excitement and anticipation. The fate of sporting supremacy hangs in the balance, and the eyes of the world are fixated on this monumental encounter.
+
+Chapter 5: The Battle Unfolds
+
+As the opening whistle pierces the silence, both teams surge forward, displaying an array of skills and tactics honed through countless hours of practice. The Stellar Strikers, determined to defend their legacy, employ their trademark precision passing, seeking to unlock Havenwood's resolute defense. Havenwood Thunderbolts, unyielding in their pursuit of victory, counter with a blistering pace and mesmerizing attacking prowess, constantly testing Stellarville's defensive resolve.
+
+The match unfolds in a symphony of dramatic moments—a jaw-dropping save by Stellarville's goalkeeper, a lightning-fast counterattack from Havenwood, and a gravity-defying bicycle kick that ricochets off the crossbar. The pendulum of dominance swings back and forth, with neither side willing to yield an inch. The tension mounts, and the stadium reverberates with thunderous roars, echoing the collective anticipation gripping the spectators.
+
+Chapter 6: The Climactic Moment
+
+In the dying embers of the game, with the scoreline deadlocked, the crescendo of excitement reaches its zenith. The final whistle looms, and a collective breath is held. And then, in a moment of breathtaking brilliance, Stellarville's star striker, Jason "The Magician" Rodriguez, conjures up a magical piece of skill. A deft touch
+
+
+
+
+
+      </section>
     </main>
   )
 }
